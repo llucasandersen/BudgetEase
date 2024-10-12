f8eca524d03830c90e8b1b18dcb9c876ad52b758
BuildTools <unconfigured@null.spigotmc.org>
2024-10-12 14:21:10 -0500

    Inital Commet

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "Lucas FBLA 2025 Banking App"
+include(":app")
